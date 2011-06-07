cbc9aee65c67db0d9021608cd7e15f9c479aa83d
chudov <devnull@localhost>
2011-06-07 19:59:07 +0000

    Removed outdated files which are now kept in Mercurial repository for CTDB server

--- CUETools.CTDB/Web/table_end.php
+++
@@ -1,9 +0,0 @@
-    </td>
-    <td background="img/bg_right_border.jpg"></td>
-  </tr>
-  <tr>
-    <td width="4" height="4"><img src="img/bottom_left_corner.jpg" width="4" height="4"></td>
-    <td background="img/bg_bottom.jpg"></td>
-    <td width="4" height="4"><img src="img/bottom_right_corner.jpg" width="4" height="4"></td>
-  </tr>
-</table>
